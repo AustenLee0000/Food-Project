c0497636266f018981dac16ddd9d8e16c4da5c5d
AustenLee0000 <liyixuan0317austen@gmail.com>
2019-11-13 20:11:10 +0800

    added routes

--- Good-Food/src/app/food8/food8.component.ts
+++ Good-Food/src/app/food8/food8.component.ts
@@ -1,12 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes,
+  query,
+  stagger
+} from '@angular/animations';
 
 @Component({
   selector: 'app-food8',
+  animations:[
+    trigger('wuhuarou-appear', [
+      state('in', style({
+        transform: 'translate(0,0)',
+      })),
+      state('out', style({
+        transform: 'translate(-200px, -100px) '
+      })),
+      transition('in => out', [
+        query('div', [
+          style({transform: 'translate(0)'}),
+          stagger('0.25s', [
+            animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'translate(-200px, -100px)'})),
+          ])
+        ])
+      ]),
+      transition('out => in', [
+        query('div', [
+          style({transform: 'translate(0)'}),
+          stagger('-0.25s', [
+            animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'translate(200px , 100px)'})),
+          ])
+        ])
+      ])
+    ]),
+      trigger('soy-sauce-appear', [
+        state('in', style({
+          transform: 'translate(0,0)',
+        })),
+        state('out', style({
+          transform: 'translate(200px, 100px) '
+        })),
+        transition('in => out', [
+          query('div', [
+            style({transform: 'translate(0)'}),
+            stagger('0.25s', [
+              animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'translate(200px, 100px)'})),
+            ])
+          ])
+        ]),
+        transition('out => in', [
+          query('div', [
+            style({transform: 'translate(0)'}),
+            stagger('-0.25s', [
+              animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'translate(-200px , -100px)'})),
+            ])
+          ])
+        ])
+      ])
+  ],
   templateUrl: './food8.component.html',
   styleUrls: ['./food8.component.scss']
 })
-export class Food8Component implements OnInit {
 
+export class Food8Component implements OnInit {
+  IsOut = true;
+  
+  toggle_wuhuarou_soysauce() {
+    this.IsOut = !this.IsOut;
+  }
   constructor() { }
 
   ngOnInit() {
