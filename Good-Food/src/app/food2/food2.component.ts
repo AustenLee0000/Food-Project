dd82bb722bca791688e252983d15e926fc5f4e09
AustenLee0000 <liyixuan0317austen@gmail.com>
2019-11-17 22:18:40 +0800

    ingredient ongoing

--- Good-Food/src/app/food2/food2.component.ts
+++ Good-Food/src/app/food2/food2.component.ts
@@ -23,26 +23,27 @@ import {
       })),
       transition('in => out', [
         query('div', [
-          style({transform: 'translate(0)'}),
+          style({transform: 'translate(0)', opacity: 0}),
           stagger('0.25s', [
             group([
               animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'translate(-200px, -100px) '})),
+              animate('0.5s ease-in', style({opacity: 1}))
             ]),
           ])
         ])
       ]),
       transition('out => in', [
         query('div', [
-          style({transform: 'translate(0)'}),
+          style({transform: 'translate(0)',opacity: 1}),
           stagger('-0.25s', [
             group([
               animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'translate(200px, 100px)'})),
+              animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({opacity: 0}))
             ])
           ])
         ])
       ])
     ]),
-
     trigger('vinegar-appear', [
       state('in', style({
         transform: 'translate(0,0)',
@@ -52,20 +53,22 @@ import {
       })),
       transition('in => out', [
         query('div', [
-          style({transform: 'translate(0)'}),
+          style({transform: 'translate(0)', opacity: 0}),
           stagger('0.25s', [
             group([
               animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'translate(200px, 100px)'})),
+              animate('0.5s ease-in', style({opacity: 1}))
             ]),
           ])
         ])
       ]),
       transition('out => in', [
         query('div', [
-          style({transform: 'translate(0)'}),
+          style({transform: 'translate(0)', opacity: 1}),
           stagger('-0.25s', [
             group([
               animate('0.5s cubic-bezier(0.35, 0, 0.25, 1)', style({transform: 'translate(-200px , -100px)'})),
+              animate('0.5s ease-in', style({opacity: 0}))
             ]),
           ])
         ])
